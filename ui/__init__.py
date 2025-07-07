ac485933f1d90fb617f675e7e0e651ced4615adb
GeunHyeog <solomond.jgh@gmail.com>
2025-07-08 02:09:03 +0900

    🏗️ ui 모듈 초기화 - 사용자 인터페이스 패키지 생성

---
+++ ui/__init__.py
@@ -0,0 +1,15 @@
+"""
+솔로몬드 AI 시스템 - UI 계층
+사용자 인터페이스 모듈
+"""
+
+__version__ = "3.0.0"
+__author__ = "전근혁 (솔로몬드 대표)"
+
+from .templates import *
+from .components import *
+
+__all__ = [
+    "templates",
+    "components"
+]
